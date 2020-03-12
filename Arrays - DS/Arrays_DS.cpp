816d7cf8fa8937a05876e6f88e5a3919706995ac
ichwanabbas11 <59040657+ichwanabbas11@users.noreply.github.com>
2020-03-12 14:50:42 +0700

    Create Arrays_DS.cpp

---
+++ Arrays - DS/Arrays_DS.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int N;
+	int* arr;
+	cin >> N;
+
+	arr = new int[N];
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> arr[i];
+	}
+	for (int i = N - 1; i >= 0; i--)
+	{
+		cout << arr[i] << " ";
+	}
+
+	return 0;
+}
